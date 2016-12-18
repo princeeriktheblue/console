a21cb1530bc0cddcf5ebbf6ae97c5d7b2ff33c9c
princeeriktheblue <princeeriktheblue@gmail.com>
2016-12-18 02:31:58 -0500

    Add files via upload

---
+++ lib/datatypes/PromptResponse.java
@@ -0,0 +1,39 @@
+package lib.datatypes;
+
+import lib.ESystem;
+
+public class PromptResponse 
+{
+	public PromptResponse(int state, int response)
+	{
+		this.state = state;
+		this.response = response;
+		nonStateResponse = "" + response;
+	}
+	
+	public PromptResponse(int state, String nonStateResponse)
+	{
+		this.state = state;
+		this.response = ESystem.NONBINARY_ANSWER;
+		this.nonStateResponse = nonStateResponse;
+	}
+	
+	public int getState()
+	{
+		return state;
+	}
+	
+	public int getResponse()
+	{
+		return response;
+	}
+	
+	public String getNonStateResponse()
+	{
+		return nonStateResponse;
+	}
+
+	private final int state;
+	private final int response;
+	private final String nonStateResponse;
+}
