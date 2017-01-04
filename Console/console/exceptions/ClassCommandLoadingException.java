4cd2489c122a69455ce098a78d691f7f7d56081a
princeeriktheblue <princeeriktheblue@gmail.com>
2017-01-04 00:28:19 -0500

    Add files via upload

---
+++ Console/console/exceptions/ClassCommandLoadingException.java
@@ -0,0 +1,14 @@
+package console.exceptions;
+
+public class ClassCommandLoadingException extends EException
+{
+	private static final long serialVersionUID = 1L;
+
+	public ClassCommandLoadingException()
+	{}
+	
+	public ClassCommandLoadingException(String x)
+	{
+		super(x);
+	}
+}
